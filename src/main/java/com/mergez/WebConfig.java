6f74e928428c1265a07ce1aabf7bab524636c478
Nassim Ben Hassine <79005429+Nassimbhs@users.noreply.github.com>
2025-11-16 15:11:42 +0100

    Update WebConfig.java

--- src/main/java/com/mergez/WebConfig.java
+++ src/main/java/com/mergez/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://naje7.netlify.app")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true)
